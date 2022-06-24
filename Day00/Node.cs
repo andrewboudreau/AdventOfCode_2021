627c6526cd7628c53a59a34072f6e5571b242435
Andrew Boudreau <andrewboudreau@gmail.com>
2022-06-24 01:29:59 -0500

    add graph

---
+++ Day00/Node.cs
@@ -0,0 +1,47 @@
+﻿public class Node<T>
+{
+    private readonly List<Node<T>> neighbors;
+
+    public Node(int x, int y, T value)
+    {
+        X = x;
+        Y = y;
+        Value = value;
+        neighbors = new List<Node<T>>();
+    }
+
+    public int X { get; init; }
+    public int Y { get; init; }
+    public T Value { get; private set; }
+
+    public IEnumerable<Node<T>> Neighbors => neighbors;
+
+    public T SetValue(T value)
+        => Value = value;
+
+    public T SetValue(Func<T, T> setter)
+        => SetValue(setter(Value));
+
+    public void AddNeighbor(IEnumerable<Node<T>> neighbor)
+         => neighbors.AddRange(neighbor);
+
+    public void AddNeighbor(Node<T> neighbor)
+        => neighbors.Add(neighbor);
+
+    public void Deconstruct(out int x, out int y, out T value)
+    {
+        x = X;
+        y = Y;
+        value = Value;
+    }
+
+    public void Deconstruct(out int x, out int y)
+    {
+        x = X;
+        y = Y;
+    }
+
+    public override string ToString() => $"{X},{Y} {Value}";
+
+    public static implicit operator T(Node<T> node) => node.Value;
+}
---
+++ Day12/Program.cs
@@ -0,0 +1,40 @@
+﻿using static Day00.ReadInputs;
+
+var grid = new List<Node<Cave>>();
+var f = Read(x =>
+{
+    var parts = x.Split("-");
+    grid.Add(new Node<Cave>(1, 1, new Cave(parts[0])));
+    grid.Add(new Node<Cave>(1, 1, new Cave(parts[1])));
+});
+
+
+public class Cave
+{
+    public Cave(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+
+    public int Visited { get; private set; }
+
+    public bool Big => char.IsUpper(Name[0]);
+
+    public bool Small => !Big;
+
+    public bool Visit()
+    {
+        if (Small && Visited > 0)
+        {
+            throw new InvalidOperationException($"Cannot visit small '{Name}' cave twice.");
+        }
+
+        return ++Visited > 0;
+    }
+
+    public bool Start => Name == "start";
+
+    public bool End => Name == "end";
+}
