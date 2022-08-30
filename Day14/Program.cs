eb04b03952bbd7f62f37fa929e645017c4b1ab9d
Andrew Boudreau <andrewboudreau@gmail.com>
2022-08-29 23:49:29 -0500

    day 14 part 1 brute force solution

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,118 +1,100 @@
-﻿using System.Drawing;
+﻿using System.Collections.Immutable;
+using System.Numerics;
 using System.Text;
 
 using static Day00.ReadInputs;
 
-var paper = ReadTo<Paper>(input =>
+var polymerizer = ReadTo<Polymerizer>(input =>
 {
-    var manual = new Paper();
+    var poly = new Polymerizer();
     foreach (var line in input)
     {
         if (string.IsNullOrEmpty(line))
         {
             continue;
         }
-        else if (line.Length < 11)
+        else if (line.Contains("->"))
         {
-            manual.AddPoint(line.Split(','));
+            poly.AddRule(line.Split(" -> "));
         }
         else
         {
-            var f = line.Split('=');
-            manual.Fold(f[0][^1] == 'y', int.Parse(f[1]));
+            poly.AddTemplate(line);
         }
     }
 
-    return manual;
+    return poly;
 });
 
-Console.Write(paper);
+//Console.Write(polymerizer);
+Console.WriteLine(polymerizer.Step(10).Solution());
 
-public class Paper
+public class Polymerizer
 {
-    private bool first = true;
-    private int xmax;
-    private int ymax;
+    private string template;
+    private Dictionary<string, char> rules;
 
-    public Paper()
+    public Polymerizer()
     {
-        Points = new List<Point>();
-        Folds = new List<(bool, int)>();
+        template = string.Empty;
+        rules = new Dictionary<string, char>();
     }
 
-    public List<Point> Points { get; }
-
-    public List<(bool Horizontal, int Offset)> Folds { get; }
-
-    public void AddPoint(string[] coords)
+    public void AddTemplate(string template)
     {
-        Points.Add(new Point(int.Parse(coords[0]), int.Parse(coords[1])));
-        xmax = Math.Max(int.Parse(coords[0]), xmax);
-        ymax = Math.Max(int.Parse(coords[1]), ymax);
+        this.template = template;
     }
 
-    public void Fold(bool horizontal, int offset)
+    public void AddRule(string[] rule)
     {
-        if (horizontal)
+        rules.Add(rule[0], rule[1].First());
+    }
+
+    public Polymerizer Step(int steps = 1)
+    {
+        var buffer = new StringBuilder();
+        for (var step = 0; step < steps; step++)
         {
-            foreach (var point in Points.Where(p => p.Y > offset).ToList())
+            Console.WriteLine($"step: {step}");
+            for (var i = 0; i < template.Length - 1; i++)
             {
-                Points.Remove(point);
-                Points.Add(point with { Y = offset - (point.Y - offset) });
+                buffer.Append(template[i]);
+                buffer.Append(rules[template[i..(i + 2)]]);
             }
-            ymax = offset;
-        }
-        else
-        {
-            foreach (var point in Points.Where(p => p.X > offset).ToList())
-            {
-                Points.Remove(point);
-                Points.Add(point with { X = offset - (point.X - offset) });
-            }
-            xmax = offset;
+
+            buffer.Append(template[^1]);
+            template = buffer.ToString();
+            buffer.Clear();
         }
 
-        if (first)
+        return this;
+    }
+
+    public BigInteger Solution()
+    {
+        var counts = new Dictionary<char, BigInteger>();
+        foreach (var chr in template)
         {
-            first = false;
-            Console.WriteLine("First fold leaves {0} dots.", Points.Distinct().Count());
+            if (!counts.ContainsKey(chr))
+            {
+                counts.Add(chr, 0);
+            }
+
+            counts[chr] += 1;
         }
+
+        var sorts = counts.Values.ToImmutableSortedSet();
+        return sorts.Last() - sorts.First();
     }
 
     public override string ToString()
     {
         var builder = new StringBuilder();
-
-        builder.AppendFormat("x:{0} y:{1}", xmax, ymax);
-        builder.AppendLine();
-
-        var point = new Point();
-        for (var y = 0; y <= ymax; y++)
+        builder.AppendLine(template);
+        foreach (var (Pair, Insert) in rules)
         {
-            if (Folds.Any(f => f == (true, y)))
-            {
-                builder.Append(new string('-', xmax));
-                builder.AppendLine();
-                continue;
-            }
-
-            for (var x = 0; x <= xmax; x++)
-            {
-                if (Folds.Any(f => f == (true, x)))
-                {
-                    builder.Append('|');
-                }
-                else
-                {
-                    point.X = x;
-                    point.Y = y;
-                    builder.Append(Points.Contains(point) ? '#' : '.');
-                }
-            }
-
-            builder.AppendLine();
+            builder.AppendLine($"{Pair} -> {Insert}");
         }
-
         return builder.ToString();
     }
 }
